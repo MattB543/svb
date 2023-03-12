177a21892c3ebff19a986f3909bd95667ef10588
Matt Brooks <mbrooks@aedo.net>
2023-03-12 18:57:11 -0400

    pushing stuff

---
+++ script.js
@@ -0,0 +1,88 @@
+// decisions
+var decisions = [
+  {
+    id: 1,
+    question: "You did it! You've landed the job as an SVB executive. Do you:",
+    options: [
+      "Accept it and hope for a golden parachute",
+      "Decline it and join a crypto startup instead",
+      "Accept it but secretly work for Thiel",
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "Time to impress the VCs! Which of these strategies do you choose:",
+    options: [
+      "Give them tickets to the Warriors game and hope they don't notice Steph Curry is missing",
+      "Give them tickets to burning man and hope they don't get lost in the desert",
+      "Invite them to your house for dinner and serve them Beyond Meat",
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "You're in charge of deciding which startups get loans. What's your move?",
+    options: [
+      "Give loans to the crypto startup and get paid in NFTs",
+      "Give loans to the biotech company and get access to their experimental drugs",
+      "Give loans to the social media app and get featured as an influencer",
+    ],
+  },
+  {
+    id: 4,
+    question: "It's time to create your portfolio. What's your strategy?",
+    options: [
+      "Buy long-term bonds and hope inflation doesn't kill you",
+      "Buy short-term bonds and hope interest rates don't spike",
+      "Buy no bonds and invest in Dogecoin instead",
+    ],
+  },
+  {
+    id: 5,
+    question: "How will you respond to rising inflation and interest rates?",
+    options: [
+      "Adjust your strategy by raising fees on your customers",
+      "Ignore the news by deleting Twitter from your phone",
+      "Blame your strategy on someone else by firing your CFO",
+    ],
+  },
+  {
+    id: 6,
+    question: "Rumors of financial troubles are circulating. What do you do?",
+    options: [
+      "Deny them by issuing a press release full of buzzwords ",
+      "Admit them by going on CNBC and crying ",
+      "Sell the news to Thiel by becoming his mole inside SVB ",
+    ],
+  },
+  {
+    "id ": 7,
+    "question ":
+      "The bank is in trouble. Will you seek a buyer or try to save it yourself?",
+    options: [
+      "Seek a buyer for SVB by pitching it as an opportunity for growth ",
+      "Try to save SVB yourself by applying for a PPP loan ",
+      "Do neither and wait for Elon Musk's tweet",
+    ],
+  },
+  {
+    "id ": 8,
+    "question ": "The regulators are coming. What's your approach?",
+    options: [
+      "Cooperate with regulators by giving them access to all your records ",
+      "Resist regulators by hiding evidence in offshore accounts ",
+      "Seduce regulators by inviting them over for Netflix and chill ",
+    ],
+  },
+  {
+    "id ": 9,
+    "question ":
+      "It's time to protect your personal assets and reputation. What's your plan?",
+    options: [
+      "Cash out by selling all your shares before they tank ",
+      "Stay loyal by buying more shares at bargain prices ",
+      "Flee the country by booking a flight on SpaceX",
+    ],
+  },
+];
