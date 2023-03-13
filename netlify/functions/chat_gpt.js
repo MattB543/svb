b8dc119b5381dbdd68dac9922e9395d8369ff442
Matt Brooks <mbrooks@aedo.net>
2023-03-13 12:37:48 -0400

    v1 is live

---
+++ netlify/functions/chat_gpt.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+exports.handler = async function (event) {
+  const payload = JSON.parse(event.body);
+  const messages = payload.messages;
+
+  var bearer = "Bearer " + process.env.OPENAI_API_KEY;
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization: bearer,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0.2,
+    }),
+  });
+
+  const data = await response.json();
+
+  return {
+    statusCode: response.status,
+    body: JSON.stringify(data),
+  };
+};
