c1106b30308113c140b79d734e705902de5742cf
Matt Brooks <mbrooks@aedo.net>
2023-03-14 09:34:36 -0400

    showing answers to create 4th answer

--- netlify/functions/chat_gpt.js
+++ netlify/functions/chat_gpt.js
@@ -14,7 +14,7 @@ exports.handler = async function (event) {
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
       messages: messages,
-      temperature: 1,
+      temperature: 0.9,
     }),
   });
 
